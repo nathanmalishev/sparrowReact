fc47e42cf1be81a29095e686568efc058287ecea
Hao Le <hao.h.le@gmail.com>
2016-05-02 22:42:21 +1000

    Added Sign in with Google button

--- app/containers/PasswordContainer.js
+++ app/containers/PasswordContainer.js
@@ -58,9 +58,15 @@ class PasswordContainer extends Component{
             
             <button className="btn btn-primary btn-block btn-lg btn-signin" type="submit" value="Post">Sign In</button>
             <button className="btn btn-danger btn-block btn-lg btn-signup" type="submit" value="Post">Sign Up</button>
+
             <a className="btn btn-block btn-social btn-facebook">
               <span className="fa fa-facebook"></span>
               Sign in with Facebook
+            </a>
+            
+            <a className="btn btn-block btn-social btn-google">
+              <span className="fa fa-google"></span>
+              Sign in with Google
             </a>
           
           </form>
@@ -72,32 +78,6 @@ class PasswordContainer extends Component{
       );
   }
 }
-
-/*
-<body>
-    <div class="container"><span class="reauth-email"> </span>
-        <h1 class="text-nowrap text-center"> SPARROW </h1></div>
-    <div class="login-card">
-        <img src="avatar_2x.png" class="profile-img-card" />
-        <p class="profile-name-card"> </p>
-        <form class="form-signin"><span class="reauth-email"> </span>
-            <input type="email" required placeholder="Email address" autofocus class="form-control" id="inputEmail" />
-            <input type="password" required placeholder="Password" class="form-control" id="inputPassword" />
-            <div class="checkbox">
-                <div class="checkbox">
-                    <label>
-                        <input type="checkbox" />Remember me</label>
-                </div>
-            </div>
-            <button class="btn btn-primary btn-block btn-lg btn-signin" type="submit">Sign in</button>
-        </form><a href="#" class="forgot-password">Forgot your password?</a></div>
-</body>
-</html>
-
-*/
-
-
-
 
 PasswordContainer.propTypes = {
   handleLoginClick: PropTypes.func.isRequired,
