2d55e897d7495382c16c10d244ef15a0d179f72a
Hao Le <hao.h.le@gmail.com>
2016-05-02 21:46:33 +1000

    Installed react-font-awesome Populated main.scss Initial styling of the login page

--- app/containers/PasswordContainer.js
+++ app/containers/PasswordContainer.js
@@ -31,29 +31,67 @@ class PasswordContainer extends Component{
 
   render() {
     return (
-        <div>
-        username: nathan
-        password: test
-          <form onSubmit={this.handleSubmit}>
-            Enter Username
+        <div className="login-card">
+          <form className="form-signin" onSubmit={this.handleSubmit}>
             <input
               type="text"
+              placeholder="Username = nathan"
+              autofocus className="form-control"
               value={this.state.username}
               onChange={this.handleUserChange}
             />
-            Enter Password
+            
+            <div className="divider" />
+            
             <input
               type="text"
+              placeholder="Password = test"
+              className="form-control"
               value={this.state.password}
               onChange={this.handlePassChange}
             />
-
-            <input type="submit" value="Post" />
+            
+            <div className="checkbox">
+              <label>
+                <input type="checkbox" />Remember me</label>
+            </div>
+            
+            <button className="btn btn-primary btn-block btn-lg btn-signin" type="submit" value="Post">Sign in</button>
+            <button className="btn btn-danger btn-block btn-lg btn-signin" type="submit" value="Post">Sign up</button>
+          
           </form>
+          
+          <a href="#" className="forgot-password">Forgot your password?</a>     
         </div>
       );
   }
 }
+
+/*
+<body>
+    <div class="container"><span class="reauth-email"> </span>
+        <h1 class="text-nowrap text-center"> SPARROW </h1></div>
+    <div class="login-card">
+        <img src="avatar_2x.png" class="profile-img-card" />
+        <p class="profile-name-card"> </p>
+        <form class="form-signin"><span class="reauth-email"> </span>
+            <input type="email" required placeholder="Email address" autofocus class="form-control" id="inputEmail" />
+            <input type="password" required placeholder="Password" class="form-control" id="inputPassword" />
+            <div class="checkbox">
+                <div class="checkbox">
+                    <label>
+                        <input type="checkbox" />Remember me</label>
+                </div>
+            </div>
+            <button class="btn btn-primary btn-block btn-lg btn-signin" type="submit">Sign in</button>
+        </form><a href="#" class="forgot-password">Forgot your password?</a></div>
+</body>
+</html>
+
+*/
+
+
+
 
 PasswordContainer.propTypes = {
   handleLoginClick: PropTypes.func.isRequired,
