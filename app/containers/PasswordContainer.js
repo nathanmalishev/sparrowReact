2abc05cc2c05f395824278c4ee1b23e25c437ac3
Nathan Malishev <nathan.malishev@gmail.com>
2016-05-02 22:50:06 +1000

    merge

--- app/containers/PasswordContainer.js
+++ app/containers/PasswordContainer.js
@@ -65,6 +65,17 @@ class PasswordContainer extends Component{
               callback={this.props.onGoogleClick}
             />
 
+
+            <a className="btn btn-block btn-social btn-facebook">
+              <span className="fa fa-facebook"></span>
+              Sign in with Facebook
+            </a>
+
+            <a className="btn btn-block btn-social btn-google">
+              <span className="fa fa-google"></span>
+              Sign in with Google
+            </a>
+
           </form>
 
           <br/>
@@ -74,32 +85,6 @@ class PasswordContainer extends Component{
       );
   }
 }
-
-/*
-<body>
-    <div class="container"><span class="reauth-email"> </span>
-        <h1 class="text-nowrap text-center"> SPARROW </h1></div>
-    <div class="login-card">
-        <img src="avatar_2x.png" class="profile-img-card" />
-        <p class="profile-name-card"> </p>
-        <form class="form-signin"><span class="reauth-email"> </span>
-            <input type="email" required placeholder="Email address" autofocus class="form-control" id="inputEmail" />
-            <input type="password" required placeholder="Password" class="form-control" id="inputPassword" />
-            <div class="checkbox">
-                <div class="checkbox">
-                    <label>
-                        <input type="checkbox" />Remember me</label>
-                </div>
-            </div>
-            <button class="btn btn-primary btn-block btn-lg btn-signin" type="submit">Sign in</button>
-        </form><a href="#" class="forgot-password">Forgot your password?</a></div>
-</body>
-</html>
-
-*/
-
-
-
 
 PasswordContainer.propTypes = {
   handleLoginClick: PropTypes.func.isRequired,
