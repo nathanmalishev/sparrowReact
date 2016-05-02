148d1e1c465ef1ccb2eb68eb36e8fb614f921387
Nathan Malishev <nathan.malishev@gmail.com>
2016-05-02 11:14:08 +1000

    lists usernames and destinations on home page

--- app/components/GroupTab.js
+++ app/components/GroupTab.js
@@ -17,12 +17,21 @@ const styles = {
 
 
 export default function Group ({_id,groupname, users,destinations}) {
+
+  const destinationList = destinations.map((dest)=>{
+    return dest.name.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();}) + ' '
+  })
+  const userList = users.map((user)=>{
+    return user.username.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();}) + ' '
+  })
   return (
     <Link to={'/group/'+_id} >
       <div style={styles.container}>
         <p>Name: {groupname}</p>
         <p>Destinations: {destinations.length}</p>
+        <p>{destinationList}</p>
         <p>Users: {users.length}</p>
+        <p>{userList}</p>
       </div>
     </Link>
   )
