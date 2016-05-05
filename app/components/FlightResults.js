d0a390d8d6d9a90440123554a5bfc513441f57de
Hao Le <leh2@student.unimelb.edu.au>
2016-05-05 13:36:37 +1000

    WIP - Rome2Rio API calls and results

--- app/components/FlightResults.js
+++ app/components/FlightResults.js
@@ -1,11 +1,30 @@
-import React, { PropTypes } from 'react'
-export default function FlightResults ({to, from, data}) {
-  return (
-    <div>
-      <p>Flight resulsts</p>
-      <p>To: {to}</p>
-      <p>From: {from}</p>
-      <p>Data: {JSON.stringify(data.agencies)}</p>
-    </div>
-  )
+import React, { Component } from 'react'
+import {Accordion, Panel} from 'react-bootstrap'
+export default class FlightResults extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    var count = 0;
+    return (
+      <Accordion>
+      <br/>
+      {this.props.data.routes.map(function(route) {
+
+        var segs = route.segments.map(function(seg) {
+          return <li>{seg.kind}</li>;
+        });
+        return (
+          <Panel header={route.name} eventKey={count++}>
+            <p> Duration : {route.duration} minutes, Price: ${route.indicativePrice.price} </p>
+            <ul>{segs}</ul>
+          </Panel>
+        );
+      })
+      }
+      </Accordion>
+    )
+  }
 }
