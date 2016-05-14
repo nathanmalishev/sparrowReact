095c4adf8f161db883102421f309bc91f31aa743
Nathan Malishev <nathan.malishev@gmail.com>
2016-05-14 16:55:03 +1000

    added css for reactleaflet

--- app/containers/ItineraryContainer.js
+++ app/containers/ItineraryContainer.js
@@ -1,40 +1,30 @@
 import React, { Component } from 'react';
-import {Link} from 'react-router'
-import { render } from 'react-dom'
-import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 
-const position = [51.505, -0.09];
-// const map = (
-//     <Map center={position} zoom={13}>
-//       <TileLayer
-//         url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
-//         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-//       />
-//       <Marker position={position}>
-//         <Popup>
-//           <span>A pretty CSS3 popup.<br/>Easily customizable.</span>
-//         </Popup>
-//       </Marker>
-//     </Map>
-// );
+export default class SimpleExample extends Component {
+  constructor() {
+    super();
+    this.state = {
+      lat: 51.505,
+      lng: -0.09,
+      zoom: 13,
+    };
+  }
 
-// render(map, document.getElementById('map-container'));
-
-export default class ItineraryContainer extends Component {
   render() {
-    return(
-      <Map center={position} zoom={13}>
+    const position = [this.state.lat, this.state.lng];
+    return (
+      <Map center={position} zoom={this.state.zoom}>
         <TileLayer
-          url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
         />
         <Marker position={position}>
           <Popup>
-            <span>A pretty CSS3 popup.<br/>Easily customizable.</span>
+            <span>A pretty CSS3 popup. <br/> Easily customizable.</span>
           </Popup>
         </Marker>
       </Map>
     );
   }
 }
-
