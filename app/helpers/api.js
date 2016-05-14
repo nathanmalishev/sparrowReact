d15f790fdac22b24e8622c6cd049b933738d6125
Nathan Malishev <nathan.malishev@gmail.com>
2016-05-14 17:05:26 +1000

    Readded getExpenses API call

--- app/helpers/api.js
+++ app/helpers/api.js
@@ -93,6 +93,21 @@ export function postExpenses(groupId,expenses) {
   });
 }
 
+export function getExpenses(groupId) {
+  const token = window.localStorage.getItem('sparrow-travel-token');
+  const url = `${_baseURL}api/groups/${groupId}/expenses?access_token=${token}`;
+  return axios.get(url)
+    .then((res)=> {
+      if (res === undefined) {
+        throw 'response is undefined';
+      }
+
+      return res;
+    })
+    .catch((err)=> {
+      console.log(err);
+    });
+}
 
 
 export function putExpenses(groupId,expenses) {
